951e9520b7df63920ac3f2bbe4703806fec6044c
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2021-02-08 00:06:04 +0900

    :wrench: Composition APIの設定を追加

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -3,6 +3,12 @@ div.SiteLayout
   nuxt
 </template>
 
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api"
+
+export default defineComponent({})
+</script>
+
 <style lang="stylus">
 .SiteLayout
   min-height: 100vh
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,6 +3,12 @@ div.SiteIndex
   h1 aycfz-homepage
 </template>
 
+<script lang="ts">
+import { defineComponent } from "@nuxtjs/composition-api"
+
+export default defineComponent({})
+</script>
+
 <style lang="stylus">
 .SiteIndex
   min-height: 100vh
