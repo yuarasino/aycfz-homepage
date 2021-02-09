1528e40bb12bb540414b0d77c606fe8d2d394f06
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2021-02-09 22:09:30 +0900

    :art: リストがふわっと出てくるアニメーションを追加

--- src/components/sections/SiteHeader.vue
+++ src/components/sections/SiteHeader.vue
@@ -51,6 +51,7 @@ export default defineComponent({
   position: fixed
   top: 0
   left: 0
+  z-index: 9999
   width: 100%
   height: 4rem
   transition: 1s
--- src/components/sections/index/IndexNews.vue
+++ src/components/sections/index/IndexNews.vue
@@ -8,16 +8,33 @@ section.IndexNews#news
         li.summary-item(v-for="post in posts" :key="post.slug")
           NuxtLink.summary-link(:to="`/news/${post.slug}`")
             NewsSummary.summary(:post="post")
+  button(@click="onClick") 追加
 </template>
 
 <script lang="ts">
-import { defineComponent, PropType } from "@nuxtjs/composition-api"
+import { defineComponent, PropType, onMounted } from "@nuxtjs/composition-api"
 import { INewsPost } from "@/types/news"
 
 export default defineComponent({
   props: {
     posts: { type: Array as PropType<INewsPost[]>, required: true },
   },
+  setup: () => {
+    onMounted(() => {
+      const els = document.querySelectorAll(".summary-item")
+      els.forEach(el => {
+        const f = () => {
+          const top = el.getBoundingClientRect().top
+          const wh = window.innerHeight
+          if (wh - top > 64) {
+            el.classList.add("scroll-appear")
+            window.removeEventListener("scroll", f)
+          }
+        }
+        window.addEventListener("scroll", f)
+      })
+    })
+  }
 })
 </script>
 
@@ -27,5 +44,13 @@ export default defineComponent({
   min-height: 100vh
 
 .summary-item
-  margin-top: 2rem
+  margin-top: 4rem
+  transition: 1s
+  opacity: 0
+  transform: translateY(4rem)
+
+.scroll-appear
+  &.summary-item
+    opacity: 1
+    transform: translateY(0)
 </style>
