8d3ea8694ff111d9879fc012549a27f1a7a66f10
新篠ゆう <37145593+yuarasino@users.noreply.github.com>
2021-07-15 03:35:34 +0900

    :wrench: リント・フォーマットの設定を追加

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "next",
+    "next/core-web-vitals",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,8 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import "@/styles/globals.css"
 
-function MyApp({ Component, pageProps }: AppProps) {
+const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
   return <Component {...pageProps} />
 }
+
 export default MyApp
